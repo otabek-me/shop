dcd3fd9577bb06eb3336cc6a2e9f29db5205ba5e
victus <otabekrahmiddinov08@gmail.com>
2026-06-09 12:55:44 +0500

    chore: .gitignore sozlandi va kesh fayllar o'chirildi

--- staticfiles/js/home.js
+++
@@ -1,5 +0,0 @@
-// home.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Agar qo‘shimcha interaktivlik kerak bo‘lsa, shu yerga yozing.
-    console.log('Dashboard yuklandi');
-});
